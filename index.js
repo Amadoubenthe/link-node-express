f8a781ccbb9c2d5f0ef200db92ad539e7b5e80ea
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2025-01-21 15:38:06 +0000

    (feat) add new post

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Post = require("./models/post.model");
+
+dotenv.config();
 const app = express();
-const port = 3000;
-const connexionString = "mongodb://localhost:27017";
+app.use(express.json());
+
+const port = process.env.PORT | 8000;
+const dbUrl = process.env.DB_URL;
 
 app.get("/", (_, res) => {
   res.send("Hello World express node !");
+});
+
+app.post("/api/posts", async (req, res) => {
+  try {
+    const p = new Post(req.body);
+    const postSaved = await p.save();
+
+    res.status(201).json(postSaved);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
 });
 
 app.listen(port, () => {
@@ -13,6 +31,6 @@ app.listen(port, () => {
 });
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/test")
+  .connect(dbUrl)
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log("Err: ", err));
