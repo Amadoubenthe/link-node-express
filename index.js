6ebf6ff9fd95876e4b2a318bb450ea0f74826425
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2025-01-21 15:15:12 +0200

    Connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+const connexionString = "mongodb://localhost:27017";
 
 app.get("/", (_, res) => {
   res.send("Hello World express node !");
@@ -9,3 +11,8 @@ app.get("/", (_, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("Err: ", err));
